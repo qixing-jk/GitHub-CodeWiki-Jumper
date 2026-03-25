804a55a279b969649c08d6fb719c253df69882d8
anime <street-anime-olive@duck.com>
2026-03-25 23:04:09 +0800

    fix: make sidebar button injection resilient to GitHub async navigation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,5 @@
 import { addButtons } from './utils/dom'
 import { setupNavigationListeners } from './utils/events'
 
-// Initial run
-addButtons()
-
-// Setup navigation listeners
 setupNavigationListeners()
+addButtons()
--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,21 +1,48 @@
+import { JUMPER_CONTAINER_ID, INJECTION_SELECTOR } from '@/constants'
 import { addButtons } from './dom'
 
-const handleNavigation = () => {
-  setTimeout(addButtons, 500)
+let injectTimer: ReturnType<typeof setTimeout> | undefined
+
+const shouldInjectButtons = () => {
+  return !!document.querySelector(INJECTION_SELECTOR) && !document.getElementById(JUMPER_CONTAINER_ID)
+}
+
+const scheduleInjection = (delay = 200) => {
+  if (injectTimer) {
+    clearTimeout(injectTimer)
+  }
+
+  injectTimer = setTimeout(() => {
+    injectTimer = undefined
+    if (shouldInjectButtons()) {
+      addButtons()
+    }
+  }, delay)
+}
+
+const observeSidebar = () => {
+  const observer = new MutationObserver(() => {
+    if (shouldInjectButtons()) {
+      scheduleInjection(0)
+    }
+  })
+
+  observer.observe(document.documentElement, {
+    childList: true,
+    subtree: true,
+  })
 }
 
 export const setupNavigationListeners = () => {
-  const originalPushState = history.pushState
-  history.pushState = function (...args) {
-    originalPushState.apply(this, args)
-    handleNavigation()
+  const handleNavigation = () => {
+    scheduleInjection()
   }
 
-  const originalReplaceState = history.replaceState
-  history.replaceState = function (...args) {
-    originalReplaceState.apply(this, args)
-    handleNavigation()
-  }
-
+  document.addEventListener('turbo:load', handleNavigation)
+  document.addEventListener('turbo:render', handleNavigation)
+  document.addEventListener('pjax:end', handleNavigation)
   window.addEventListener('popstate', handleNavigation)
+
+  observeSidebar()
+  scheduleInjection(0)
 }
